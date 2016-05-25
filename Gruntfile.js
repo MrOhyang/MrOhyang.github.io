c336f209d67bca533eab79512addfa2b31211f2a
MrOhyang <ohyang94e@163.com>
2016-05-25 16:36:51 +0800

    配置grunt自动构建工具

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,39 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
+        
+        // Task
+        sass: {
+            dist: {
+                options: {
+                    sourcemap: 'none',
+                    // compact 一行, compressed 压缩, expanded 展开
+                    style: 'expanded'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'sass',
+                    src: ['**/*.scss'],
+                    dest: 'css/',
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            scripts: {
+                files: [
+                    'sass/**/*.scss'
+                ],
+                task: ['sass']
             }
         }
     });
 
-    // 加载包含 "uglify" 任务的插件。
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // 加载包含 "Tast" 任务的插件。
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // 默认被执行的任务列表。
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['sass', 'watch']);
 
 };
