d13d2070cd56512f53b98af8fbb45cd99dbb87ca
Ouyang Yibin <ohyang94e@163.com>
2017-04-15 00:39:42 +0800

    优化代码

--- ASaima/src/HelloLayabox.js
+++ ASaima/src/HelloLayabox.js
@@ -5,90 +5,70 @@ var Browser = Laya.Browser;
 var Handler = Laya.Handler;
 var WebGL   = Laya.WebGL;
 
+init();  // Laya 环境 初始化
 
+var horse_list = [];
 
+// 创建马匹
+horse_list.push(initHorse(0, 40));
+horse_list.push(initHorse(0, 140));
+horse_list.push(initHorse(0, 240));
 
-// 不支持WebGL时自动切换至Canvas
-Laya.init(Browser.clientHeight, Browser.clientWidth, WebGL);
-// Laya.init(Browser.clientWidth, Browser.clientHeight, WebGL);
+Laya.timer.frameLoop(1, this, run);  // 开始赛马
 
-console.log(Browser.clientWidth, Browser.clientHeight);
+// Laya 环境 初始化
+function init() {
+    Laya.init(Browser.clientHeight, Browser.clientWidth, WebGL);
+    Laya.stage.alignV = Stage.ALIGN_MIDDLE;
+    Laya.stage.alignH = Stage.ALIGN_CENTER;
 
-Laya.stage.alignV = Stage.ALIGN_MIDDLE;
-Laya.stage.alignH = Stage.ALIGN_CENTER;
+    Laya.stage.scaleMode = "showall";
+    Laya.stage.screenMode = Stage.SCREEN_HORIZONTAL;
+    Laya.stage.bgColor = "#eee";
+}
 
-Laya.stage.scaleMode = "showall";
-Laya.stage.screenMode = Stage.SCREEN_HORIZONTAL;
-Laya.stage.bgColor = "#eee";
-
-var hourse = initObject(100);
-var hourse2 = initObject(200);
-
-run();
-
-function initObject(_y) {
+// 马匹 初始化
+function initHorse(_x, _y) {
     var obj = {
-        x: 72,
-        y: _y,
+        o: {x: 144 / 2, y: 155 / 2},
+        x: _x ? _x : 0,
+        y: _y ? _y : 0,
         du: 0,
         xuan: 0,
+        x_add: ~~(Math.random() * 3) + 1,
+        du_add: ~~(Math.random() * 11) + 5,
         ape: new Sprite()
     };
 
-    obj.ape.loadImage('images/hourse.png', obj.x, obj.y);
-    obj.ape.pivot(144/2+obj.x, (117+obj.y)/2);
+    obj.ape.loadImage('images/hourse.png', 0, 0);
+    obj.ape.pivot(obj.o.x, obj.o.y);
+    obj.x += obj.o.x;
+    obj.y += obj.o.y;
+    obj.ape.pos(obj.x, obj.y);
     Laya.stage.addChild(obj.ape);
     return obj;
 }
 
-function hourseRun() {
-    // hourse.x += (Math.random() * 2) + 1;
-    // hourse2.x += (Math.random() * 3) + 1;
-
-    hourse.x += 2;
-    hourse2.x += 2;
-
-    hourse.y -= 3 * Math.sin(hourse.du * 0.0174);
-    hourse.du += 10;
-    
-    hourse.ape.rotation = 30 * Math.sin(hourse.du * 0.0174);
-    hourse.ape.pos(hourse.x, hourse.y);
-    hourse2.ape.pos(hourse2.x, hourse2.y);
-
-    if (hourse.x > Browser.clientHeight - 72) {
-        hourse.x = Browser.clientHeight - 72;
-        Laya.timer.clear(this, hourseRun);
-    }
-    if (hourse2.x > Browser.clientHeight - 72) {
-        hourse2.x = Browser.clientHeight - 72;
-        Laya.timer.clear(this, hourseRun);
-    }
-}
-
+// 开始赛马
 function run() {
-    Laya.timer.frameLoop(1, this, hourseRun);
+    clearFlag = false;
+    horse_list.forEach(function(horse, i) {
+        horse.x += horse.x_add;
+        horse.y -= 3 * Math.sin(horse.du * 0.0174);
+        horse.du += horse.du_add;
+
+        // 判断是否结束
+        if (horse.x >= Browser.clientHeight - 80) {
+            clearFlag = true;
+            horse.x = Browser.clientHeight - 80;
+        }
+
+        // 动画
+        horse.ape.rotation = 26 * Math.sin(horse.du * 0.0174);
+        horse.ape.pos(horse.x, horse.y);
+    });
+
+    if (clearFlag) {
+        Laya.timer.clear(this, run);
+    }
 }
-
-// showApe();
-// setup();
-
-function showApe()
-{
-    // 方法1：使用loadImage
-    // ape.loadImage("images/hourse.png", x, 10);
-    ape.pos(x, 10);
-}
-
-console.log();
-
-// function setup() {
-//     Laya.timer.frameLoop(1, this, function () {
-//         x += 3;
-//         console.log(x, Laya.Browser.clientWidth);
-//         if (x > Laya.Browser.clientWidth) {
-//             console.log('>');
-//             Laya.timer.clearAll(this);
-//         }
-//         showApe();
-//     });
-// }
